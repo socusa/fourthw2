3cf54bbd44931abe54ee420bdd9949ff7497bd7b
Keith Lynn <klynn@southalabama.edu>
2019-06-13 19:06:00 +0100

    Commit

--- app/src/main/java/com/example/lynn/fourth/MyView.java
+++ app/src/main/java/com/example/lynn/fourth/MyView.java
@@ -41,11 +41,11 @@ public class MyView extends RelativeLayout {
 
         invalidate();
 
-        state = new StateView(context,widthOfRectangle - 10,300);
+        state = new StateView(context,widthOfRectangle - 50,300);
 
         state.getState();
 
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(widthOfRectangle-10,300);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(widthOfRectangle-50,300);
 
         state.setLayoutParams(layoutParams);
 
