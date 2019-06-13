78871326d35485588e3ed36e265997408a6f83b6
Keith Lynn <klynn@southalabama.edu>
2019-06-13 20:06:08 +0100

    Commit

--- app/src/main/java/com/example/lynn/fourth/MyListener.java
+++ app/src/main/java/com/example/lynn/fourth/MyListener.java
@@ -1,8 +1,11 @@
 package com.example.lynn.fourth;
 
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
+
+import static com.example.lynn.fourth.MainActivity.*;
 
 public class MyListener implements View.OnTouchListener {
     private int offsetX;
@@ -20,6 +23,12 @@ public class MyListener implements View.OnTouchListener {
             params.topMargin = (int)(motionEvent.getRawY() - offsetY);
 
             view.setLayoutParams(params);
+
+            Rect rectangle = new Rect(params.leftMargin,params.topMargin,params.leftMargin + view.getWidth(),params.topMargin + view.getHeight());
+
+            if (rectangles[0].contains(rectangle))
+                message.setText("Contains");
+
         }
 
         return (true);
